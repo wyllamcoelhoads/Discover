ee060c021c2fb97d1836ff145c77829ec1a6aa53
William Coelho <wyllamcoelho8@gmail.com>
2024-03-21 22:28:22 -0300

    Dividindo o codigo em arquivos

---
+++ script.js
@@ -0,0 +1,3 @@
+// Substitua os valores abaixo pelos seus próprios dados
+document.getElementById('nome').innerText = 'William';
+document.getElementById('email').innerText = 'williamcoelho8@mail.com';
